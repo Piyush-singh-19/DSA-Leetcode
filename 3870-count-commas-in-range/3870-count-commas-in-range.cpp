0e895b795d68f9f77436cab02f3416baaf47c86c
Piyush-singh-19 <piyush.vns19@gmail.com>
2026-09-09 00:31:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int countCommas(int n) {
-        long comma=0;
-        for(int i=1000;i<=n;i++){
-            comma++;
-        }
-        return comma;
-        
+        // long comma=0;
+        // for(int i=1000;i<=n;i++){
+        //     comma++;
+        // }
+        // return comma;
+        int count=0;
+
+        if(n<=999){
+        count=0;}
+        else{
+        count=n-999;
+        }        
+    
+    return count;
     }
 };
